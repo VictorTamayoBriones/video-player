7b3fa968b5c42aedafa5409b390223d9fecbe22a
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-01-28 23:34:53 -0600

    player styles

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const $video = document.querySelector('#video');
 const $play = document.querySelector('#play');
 const $pause = document.querySelector('#pause');
+const $backward = document.querySelector('#backward');
+const $forward = document.querySelector('#forward');
+
 
 const handlePlay = ()=>{
     $video.play();
@@ -11,10 +14,22 @@ const handlePlay = ()=>{
 
 const handlePause = ()=>{
     $video.pause();
-    
+
     $play.hidden = false;
     $pause.hidden = true;
 }
 
+const handleBackward = ()=>{
+    console.log('back', $video.currentTime);
+    $video.currentTime -= 10;
+}
+
+const handleForward = ()=>{
+    console.log('for', $video.currentTime);
+    $video.currentTime += 10;
+}
+
 $play.addEventListener('click', handlePlay);
 $pause.addEventListener('click', handlePause);
+$backward.addEventListener('click', handleBackward);
+$forward.addEventListener('click', handleForward);
